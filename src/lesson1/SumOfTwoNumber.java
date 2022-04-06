8663b842dd645565065b678ec93439b9091bd266
bayuguai <2522706918@qq.com>
2022-04-06 09:55:17 +0800

    两数之和

--- src/lesson1/SumOfTwoNumber.java
+++ src/lesson1/SumOfTwoNumber.java
@@ -11,6 +11,13 @@ import java.util.HashMap;
 public class SumOfTwoNumber {
     public static void main(String[] args) {
 
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int[] solution1result = SumOfTwoNumber.solution1(nums,target);
+        System.out.println("solution1result:"+solution1result[0]+","+solution1result[1]);
+
+        int[] solution2result = SumOfTwoNumber.solution2(nums,target);
+        System.out.println("solution2result:"+solution2result[0]+","+solution2result[1]);
     }
 
     private static int[] solution1(int[] nums,int target){
@@ -36,6 +43,6 @@ public class SumOfTwoNumber {
                 }
                 map.put(nums[i], i );
         }
-        return null;
+       throw new IllegalArgumentException("数组中不存在对应的两数之和");
     }
 }
